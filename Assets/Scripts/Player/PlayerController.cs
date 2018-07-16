ea12b6116877729243bd0767b253f99c753e67ba
Diana Regalbuto <diana.m.regalbuto@gmail.com>
2018-07-16 17:12:08 -0400

    sprite flipping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,38 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    
+public class PlayerController : MonoBehaviour
+{
+
     public float moveSpeed = 1f;
     private Rigidbody2D body;
+    private SpriteRenderer spriteRenderer;
+    private bool facingLeft;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         body = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per physics tick
-	void FixedUpdate () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        facingLeft = true;
+    }
+
+    // Update is called once per physics tick
+    void FixedUpdate()
+    {
         float xInput = Input.GetAxisRaw("Horizontal");
         float yInput = Input.GetAxisRaw("Vertical");
 
-        if (IsMoving(xInput) || IsMoving(yInput)) {
-           // var moveVector = new Vector3(xInput, yInput, 0);
+        if (IsMoving(xInput) || IsMoving(yInput))
+        {
+            // var moveVector = new Vector3(xInput, yInput, 0);
+            if (xInput > 0 && facingLeft || xInput < 0 && !facingLeft) {
+                facingLeft = !facingLeft;
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
             body.MovePosition(new Vector2(NewX(xInput), NewY(yInput)));
         }
-	}
+    }
 
-    bool IsMoving(float x) {
+    bool IsMoving(float x)
+    {
         return x - 0 < float.Epsilon;
     }
 
-    float NewX(float x){
+    float NewX(float x)
+    {
         return transform.position.x + x * moveSpeed * Time.deltaTime;
     }
 
-    float NewY(float y) {
+    float NewY(float y)
+    {
         return transform.position.y;
         //return transform.position.y + y * moveSpeed * Time.deltaTime;
     }
+
 }
